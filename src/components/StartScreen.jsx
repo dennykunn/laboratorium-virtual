5f300e639a081c61edf774ea7d42896083a08742
putrinurbintang <putrinurbintang6@gmail.com>
2026-05-09 17:34:17 +0700

    Update StartScreen.jsx

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -60,8 +60,8 @@ function StartScreen() {
           initial={{ y: -30, opacity: 0 }} animate={{ y: 0, opacity: 1 }}
           transition={{ delay: 0.4, duration: 0.5 }} />
 
-        <motion.img src="/assets/elemen/IPA- SMP.png" alt="IPA SMP/MTs Kelas VII"
-          className="w-[min(400px,50vw)] h-auto drop-shadow-[2px_4px_8px_rgba(0,0,0,0.3)]"
+        <motion.img src="/assets/elemen/IPA_SMP.png" alt="IPA SMP/MTs Kelas VII"
+          className="w-[min(250px,50vw)] h-auto drop-shadow-[2px_4px_8px_rgba(0,0,0,0.3)]"
           initial={{ opacity: 0 }} animate={{ opacity: 1 }}
           transition={{ delay: 0.6 }} />
 
