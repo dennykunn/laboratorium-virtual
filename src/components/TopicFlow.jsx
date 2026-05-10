d7caf1f7786f3247b04ceee6ccf0858ce40a42ab
putrinurbintang <putrinurbintang6@gmail.com>
2026-05-10 07:33:32 +0700

    Update TopicFlow.jsx

--- src/components/TopicFlow.jsx
+++ src/components/TopicFlow.jsx
@@ -591,7 +591,7 @@ const renderSlide = () => {
               <h3 className="text-center text-xl lg:text-4xl font-bold mb-3">COBA TEBAK</h3>
               <ol className="list-decimal list-inside space-y-3 text-base md:text-lg lg:text-3xl leading-snug">
                 <li>Menurutmu, apa yang menyebabkan perbedaan gerak antara mobil yang satu dengan yang lain pada video?</li>
-                <li>Jika jarak diperbesar tetapi waktu tetap, bagaimana dugaanmu tentang kelajuan?</li>
+                <li>Jika waktu perjalanan kedua mobil sama, mengapa jarak yang ditempuh bisa berbeda??</li>
                 <li>Menurut pendapatmu, faktor apa yang memengaruhi kelajuan mobil?</li>
               </ol>
             </div>
